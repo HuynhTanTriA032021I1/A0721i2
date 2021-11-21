a8193c5ef8fbc3a82f5db8ade321b69d9b3b5db4
Huỳnh Tán Trí <httri1418@gmail.com>
2021-11-22 01:32:17 +0700

    Nộp Bài 13 - 19

---
+++ B11_DSA_StackQueue/src/Bai_Tap/DaoNguocPhanTuTrongMangSoNguyenSuDungStack/main.java
@@ -0,0 +1,12 @@
+package Bai_Tap.DaoNguocPhanTuTrongMangSoNguyenSuDungStack;
+
+import static DSA_Stack_Queue.bai_tap.DaoNguocPhanTuTrongMangSoNguyenSuDungStack.ReverseDigits.reverse_number;
+
+public class main {
+    public static void main(String[] args){
+        int number = 12345;
+        System.out.println("Input :"+number);
+        System.out.println("Output:"+reverse_number(number));
+    }
+}
+
---
+++ B11_DSA_StackQueue/src/Bai_Tap/Map/main.java
@@ -0,0 +1,4 @@
+package Bai_Tap.Map;
+
+public class main {
+}
